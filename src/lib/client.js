add5e3cf3acbe959a4af6fb72e448c00aaed3fd3
suisho <suisho000000@gmail.com>
2014-06-02 23:39:34 +0900

    move

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,13 +1,10 @@
-var migawari = require("migawari")
-/*
-$(".input").on("change", function(){
-  
-})*/
-window.migawari = migawari
+var $ = require("jquery")
+var Backbone = require("backbone")
 
+var migawari = require("migawari")
+
+var Output = Backbone.view
 $("input.selector").on('keydown', function(){
   var migawarid = migawari($(this).val())
   $(".result").text(migawarid.toString())
-  
 })
-
